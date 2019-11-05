c632c69af8210f0896a7c48e9ef911a7618b197d
guihenrry <guilhermehenry60@gmail.com>
2019-11-05 00:11:16 -0300

    Add page not found

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "@/views/Home.vue";
 import Detail from "@/views/Detail.vue";
+import PageNotFound from "@/views/PageNotFound.vue";
 
 Vue.use(Router);
 
@@ -9,6 +10,10 @@ export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
+    {
+      path: "*",
+      component: PageNotFound
+    },
     {
       path: "/",
       name: "home",
